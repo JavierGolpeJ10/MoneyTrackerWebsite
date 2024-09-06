4812b8100f4488a44c1f041c3473249bb125d759
JavierGolpeJ10 <golpeju2@uwm.edu>
2024-09-05 20:17:59 -0500

    Created Create Account Page
    added CreateAccountPage in views
    cut some model fields

--- Money_Tracker_app/models.py
+++ Money_Tracker_app/models.py
@@ -8,8 +8,8 @@ class MyUser(models.Model):
     user_password = models.CharField(max_length=50)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    phone = models.CharField(max_length=50)
+    # email = models.CharField(max_length=50)
+    # phone = models.CharField(max_length=50)
 
     def get_name(self):
         return self.first_name + " " + self.last_name
@@ -20,8 +20,8 @@ class MyUser(models.Model):
     def get_last_name(self):
         return self.last_name
 
-    def get_email(self):
-        return self.email
-
-    def get_phone(self):
-        return self.phone
+    # def get_email(self):
+    #     return self.email
+    #
+    # def get_phone(self):
+    #     return self.phone
